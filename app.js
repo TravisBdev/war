b9b4145212455bf267a9ec63973f3ef85f6ff6e7
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-02 12:08:37 -0400

    created two requests to the cards api to get a new deck and draw two cards for the basic functionality of the game. Just checking to see if the response was sucessful.

---
+++ app.js
@@ -0,0 +1,20 @@
+let deckId = "";
+
+function handleClick() {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then((res) => res.json())
+    .then((data) => {
+      deckId = data.deck_id;
+    });
+}
+
+function drawCards() {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+document.querySelector(".draw-cards").addEventListener("click", drawCards);
+document.querySelector(".new-deck").addEventListener("click", handleClick);
