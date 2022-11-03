3992699ba8af13045feb23a67759d829be749787
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-03 14:58:07 -0400

    created a function to determine which player wins between the two cards shown on the page. This function is then called inside the draw event listener. Added a setTimeout to make the winner text appear slightly after the function determines a winner

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cardContainer = document.querySelector(".card-container");
-const drawBtn = document.querySelector(".drawBtn");
-const newDeckBtn = document.querySelector(".newDeckBtn");
+const drawBtn = document.querySelector(".draw-Btn");
+const newDeckBtn = document.querySelector(".newDeck-Btn");
+const winnerText = document.querySelector(".winner-text");
 let deckId = "";
 
 function handleClick() {
@@ -22,7 +23,40 @@ function drawCards() {
       cardContainer.children[1].innerHTML = `
         <img src="${data.cards[1].image}" class='card' />
       `;
+
+      const winner = whoWon(data.cards[0], data.cards[1]);
+      setTimeout(() => {
+        winnerText.textContent = winner;
+      }, 300);
     });
+}
+
+function whoWon(card1, card2) {
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1Index = values.indexOf(card1.value);
+  const card2Index = values.indexOf(card2.value);
+
+  if (card1Index > card2Index) {
+    return "Computer Wins!";
+  } else if (card1Index < card2Index) {
+    return "You Win!";
+  } else {
+    return "War!";
+  }
 }
 
 drawBtn.addEventListener("click", drawCards);
